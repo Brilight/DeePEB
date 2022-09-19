851a4ecee182713560edbb4fb63424d8e1b56f03
Brilight <qpwang@pku.edu.cn>
2022-09-19 21:11:59 +0800

    Update Models

--- Model/DeePEB_v1_single.py
+++ Model/DeePEB_v1_single.py
@@ -10,10 +10,11 @@ import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint as ckpt
 from functools import partial
 
-torch.manual_seed(3407)
-np.random.seed(3407)
+torch.manual_seed(42)
+np.random.seed(42)
 
 from tqdm import trange, tqdm
+
 
 def compl_mul3d(a, b):
     # (batch, in_channel, z, y, x), (in_channel, out_channel, z, y, x) -> (batch, out_channel, z, y, x)
@@ -60,27 +61,25 @@ class SpectralConv3d(nn.Module):
     
     
 class DeePEB(nn.Module):
-    def __init__(self, devices, modes1, modes2, modes3, channels, layers, hf_channels):
+    def __init__(self, modes1, modes2, modes3, channels, layers, hf_channels):
         super(DeePEB, self).__init__()
         
         self.channels, self.modes1, self.modes2, self.modes3 = channels, modes1, modes2, modes3
         self.layers = layers
-        self.device0, self.device1 = devices[0], devices[1]
         
         self.dsp = nn.Sequential(
-            nn.Conv3d(1, 1, kernel_size=3, stride=(1,2,2), padding=1, padding_mode='replicate')).to(self.device0)
+            nn.Conv3d(1, 1, kernel_size=3, stride=(1,2,2), padding=1, padding_mode='replicate'))
 
-        self.fn0 = nn.Sequential(nn.Linear(1, self.channels)).to(self.device0)
+        self.fn0 = nn.Sequential(nn.Linear(1, self.channels))
         
         self.activation = nn.LeakyReLU(inplace=True)
         
-        self.conv1 = SpectralConv3d(self.channels, self.channels, 
-                                    self.modes1, self.modes2, self.modes3).to(self.device0)
+        self.conv1 = SpectralConv3d(self.channels, self.channels, self.modes1, self.modes2, self.modes3)
         
-        self.w1 = nn.Sequential(nn.Conv3d(self.channels, self.channels, kernel_size=1, stride=1,)).to(self.device0)
+        self.w1 = nn.Sequential(nn.Conv3d(self.channels, self.channels, kernel_size=1, stride=1))
         
-        self.fn1 = nn.Sequential(nn.Linear(self.channels, 64)).to(self.device0)
-        self.fn2 = nn.Sequential(nn.Linear(64, 1)).to(self.device1)
+        self.fn1 = nn.Sequential(nn.Linear(self.channels, 64))
+        self.fn2 = nn.Sequential(nn.Linear(64, 1))
         
         self.usp = nn.Sequential(
             nn.Upsample(scale_factor=(1,2,2), mode='trilinear',align_corners=True))      
@@ -88,28 +87,28 @@ class DeePEB(nn.Module):
         self.hf0 = nn.Sequential(
             nn.Conv3d(1, hf_channels, kernel_size=1, stride=1),
             nn.LeakyReLU(inplace=True), 
-            nn.Conv3d(hf_channels, 1, kernel_size=1, stride=1)).to(self.device1)
+            nn.Conv3d(hf_channels, 1, kernel_size=1, stride=1))
         
         self.hf1 = nn.Sequential(
             nn.Conv3d(1, 1, kernel_size=3, stride=1, 
                       dilation=1, padding=1, padding_mode='replicate'),
             nn.LeakyReLU(inplace=True),
             nn.Conv3d(1, 1, kernel_size=3, stride=1,
-                      dilation=1, padding=1, padding_mode='replicate')).to(self.device1)
+                      dilation=1, padding=1, padding_mode='replicate'))
         
         self.hf2 = nn.Sequential(
-            nn.Conv3d(1, 1, kernel_size=3, stride=1, 
+            nn.Conv3d(1, 2, kernel_size=3, stride=1, 
                       dilation=2, padding=2, padding_mode='replicate'),
             nn.LeakyReLU(inplace=True),
-            nn.Conv3d(1, 1, kernel_size=3, stride=1,
-                      dilation=2, padding=2, padding_mode='replicate')).to(self.device1)
+            nn.Conv3d(2, 1, kernel_size=3, stride=1,
+                      dilation=2, padding=2, padding_mode='replicate'))
         
         self.hf3 = nn.Sequential(
             nn.Conv3d(1, 1, kernel_size=(1,5,5), stride=1, 
-                      dilation=1, padding=(0,2,2), padding_mode='replicate'),
+                      dilation=1, padding=(0,2,2), padding_mode='zeros'),
             nn.LeakyReLU(inplace=True),
             nn.Conv3d(1, 1, kernel_size=(1,5,5), stride=1,
-                      dilation=1, padding=(0,2,2), padding_mode='replicate')).to(self.device1)
+                      dilation=1, padding=(0,2,2), padding_mode='zeros'))
         
     def initialize(self):
         for m in self.children():
@@ -131,12 +130,10 @@ class DeePEB(nn.Module):
             x = self.conv1(x) + self.w1(x)
             x = self.activation(x)
             
-        x = self.fn1(x.permute(0, 2, 3, 4, 1))
-        x = self.activation(x).to(self.device1)
+        x = ckpt(self.fn1, x.permute(0, 2, 3, 4, 1))
+        x = self.activation(x)
         x = self.fn2(x).permute(0, 4, 1, 2, 3)
         x = self.usp(x)
-        acd = acd.to(self.device1)
-        torch.cuda.empty_cache()
-        x += self.hf0(acd) + self.hf1(acd) + self.hf2(acd) + self.hf3(acd)
+        acd.requires_grad_(True)
+        x += self.hf0(acd) + ckpt(self.hf1, acd) + ckpt(self.hf2, acd) + ckpt(self.hf3, acd)
         return x
-    
